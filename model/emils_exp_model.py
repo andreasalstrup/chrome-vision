90ac335f148e11773a4ce2a8392a15402bf9df93
Emil Brandt <73188508+emilnormann@users.noreply.github.com>
2023-03-01 10:28:32 +0100

    Now uses the cityscapes data, with MNIST data still available Currently the model with cityscapes data does not work since there is no validation/ testing because of no labels

--- model/emils_exp_model.py
+++ model/emils_exp_model.py
@@ -7,6 +7,9 @@ BAD_PC = True
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
+learning_rate = 1e-3
+epochs = 5
+
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
