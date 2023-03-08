6e838e1b3648ac4d3a5f6badd66e9ffd715801db
Andreas Knudsen Alstrup <andreas090994@gmail.com>
2023-03-08 13:13:49 +0100

    Encoder indentation fix

--- model/encoder.py
+++ model/encoder.py
@@ -7,7 +7,7 @@ class ResNet50(nn.Module):
         self.resnet = models.resnet50(pretrained=True)
         self.fc = nn.Linear(in_features=in_features, num_classes=num_classes)
 
-        def forward(self, x):
-            x = self.resnet(x)
-            x = self.fc(x)
-            return x
+    def forward(self, x):
+        x = self.resnet(x)
+        x = self.fc(x)
+        return x
