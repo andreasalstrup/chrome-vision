2c8bcb353adcf40331136615acf41f6751222c63
Andreas Knudsen Alstrup <andreas090994@gmail.com>
2023-03-08 11:34:02 +0100

    Added custom ResNet50 encoder

---
+++ model/encoder.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+import torchvision.models as models
+
+class ResNet50(nn.Module):
+    def __init__(self, num_classes, in_features):
+        super().__init__()
+        self.resnet = models.resnet50(pretrained=True)
+        self.fc = nn.Linear(in_features=in_features, num_classes=num_classes)
+
+        def forward(self, x):
+            x = self.resnet(x)
+            x = self.fc(x)
+            return x
