555286f3e389f30a18110cbd56831938964e65a1
danielalexlarsen <danielalexlarsen@gmail.com>
2023-03-09 14:59:58 +0100

    GETTING CLOSER

--- model/encoder.py
+++ model/encoder.py
@@ -5,9 +5,11 @@ class ResNet50(nn.Module):
     def __init__(self, num_classes, in_features):
         super().__init__()
         self.resnet = models.resnet50(pretrained=True)
-        self.fc = nn.Linear(in_features=in_features, num_classes=num_classes)
+        self.fc = nn.Linear(in_features=in_features, out_features=num_classes)
 
     def forward(self, x):
+        print(f"ResNet50 input shape 1: {x.shape}")
         x = self.resnet(x)
+        print(f"ResNet50 output shape 2: {x.shape}")
         x = self.fc(x)
         return x
