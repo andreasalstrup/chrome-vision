ddce766d5b54ea42f268596c250541ef281917b2
Emil Brandt <emilnormannbrandt@gmail.com>
2023-03-24 09:47:27 +0100

    Chrome cutter is now only done once

--- dataset.py
+++ dataset.py
@@ -2,53 +2,47 @@ import os
 import pandas as pd
 import cv2
 from torch.utils.data import Dataset
+from torchvision.io import read_image
 import torch
-class ChromeCutter:
-    def __init__(self, annotations_file, img_dir, name):
-        if os.path.exists(f"data/leftImg8bit/train/cut/{name}"):
-            return
-        numOfCuts = 0
-        if not(os.path.exists("data/leftImg8bit/train/cut")):
-            os.mkdir("data/leftImg8bit/train/cut")
-        os.mkdir(f"data/leftImg8bit/train/cut/{name}")
-        img_names = pd.read_csv(annotations_file)        
-        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True, trust_repo=True)
-        for i in range(len(img_names)):
-            img_path = os.path.join(img_dir, img_names.iloc[i, 0])
-            image = cv2.imread(img_path)
-            cuts = model(image)
-            for result in cuts.xyxy[0]:                
-                x1, y1, x2, y2, score, monka = result    
-                # Adding the crop to the boxes list
-                croppedImage = image[int(y1):int(y2)-1,int(x1):int(x2)].copy()
-                cv2.imwrite(f"data/leftImg8bit/train/cut/{numOfCuts}", croppedImage)
-                numOfCuts += 1
+import csv
+
+def chromeCutter(annotations_file, img_dir, name, typeOfData):
+    if os.path.exists(f"data/leftImg8bit/{typeOfData}/cut/{name}"):
+        return
+    numOfCuts = 0
+    if not(os.path.exists(f"data/leftImg8bit/{typeOfData}/cut")):
+        os.mkdir(f"data/leftImg8bit/{typeOfData}/cut")
+    os.mkdir(f"data/leftImg8bit/{typeOfData}/cut/{name}")
+    img_names = pd.read_csv(annotations_file)        
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True, trust_repo=True)
+    for i in range(len(img_names)):
+        img_path = os.path.join(img_dir, img_names.iloc[i, 0])
+        image = cv2.imread(img_path)
+        cuts = model(image)
+        for result in cuts.xyxy[0]:                
+            x1, y1, x2, y2, score, monka = result    
+            # Adding the crop to the boxes list
+            croppedImage = image[int(y1):int(y2)-1,int(x1):int(x2)].copy()
+            cv2.imwrite(f"data/leftImg8bit/{typeOfData}/cut/{name}/{numOfCuts}.jpg", croppedImage)
+            numOfCuts += 1
+    f = open(f'data/leftImg8bit/indices/{typeOfData}Index/{name}.csv', 'w', newline='')
+    writer = csv.writer(f)
+    for ind in range(numOfCuts):
+        writer.writerow([f"{ind}.jpg"])
+    f.close
                 
 class CustomImageDataset(Dataset):
     def __init__(self, annotations_file, img_dir, transform=None):        
-        img_names = pd.read_csv(annotations_file)
-        self.cutList = list()    
+        self.img_names = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
         self.transform = transform        
 
-        ##chromecut
-        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True, trust_repo=True)
-        for i in range(len(img_names)):
-            img_path = os.path.join(img_dir, img_names.iloc[i, 0])
-            image = cv2.imread(img_path)
-            cuts = model(image)
-            for result in cuts.xyxy[0]:                
-                x1, y1, x2, y2, score, monka = result    
-                # Adding the crop to the boxes list
-                croppedImage = image[int(y1):int(y2)-1,int(x1):int(x2)].copy()
-                self.cutList.append(croppedImage)
-
     def __len__(self):
-        return len(self.cutList)
+        return len(self.img_names)
 
     def __getitem__(self, idx):        
-        image = self.cutList[idx]    
+        img_path = os.path.join(self.img_dir, self.img_names.iloc[idx, 0])
+        image = read_image(img_path)        
         if self.transform:
             image = self.transform(image)
         return image.float()
-    
-ChromeCutter("","","")
