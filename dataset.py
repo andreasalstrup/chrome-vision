caafe54dc3d2a0735a2a9c58355c7e00ce4cfa2f
Emil <emilnormannbrandt@gmail.com>
2023-03-24 08:43:15 +0100

    Made branch for desktop

--- dataset.py
+++ dataset.py
@@ -3,7 +3,27 @@ import pandas as pd
 import cv2
 from torch.utils.data import Dataset
 import torch
-
+class ChromeCutter:
+    def __init__(self, annotations_file, img_dir, name):
+        if os.path.exists(f"data/leftImg8bit/train/cut/{name}"):
+            return
+        numOfCuts = 0
+        if not(os.path.exists("data/leftImg8bit/train/cut")):
+            os.mkdir("data/leftImg8bit/train/cut")
+        os.mkdir(f"data/leftImg8bit/train/cut/{name}")
+        img_names = pd.read_csv(annotations_file)        
+        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True, trust_repo=True)
+        for i in range(len(img_names)):
+            img_path = os.path.join(img_dir, img_names.iloc[i, 0])
+            image = cv2.imread(img_path)
+            cuts = model(image)
+            for result in cuts.xyxy[0]:                
+                x1, y1, x2, y2, score, monka = result    
+                # Adding the crop to the boxes list
+                croppedImage = image[int(y1):int(y2)-1,int(x1):int(x2)].copy()
+                cv2.imwrite(f"data/leftImg8bit/train/cut/{numOfCuts}", croppedImage)
+                numOfCuts += 1
+                
 class CustomImageDataset(Dataset):
     def __init__(self, annotations_file, img_dir, transform=None):        
         img_names = pd.read_csv(annotations_file)
@@ -30,3 +50,5 @@ class CustomImageDataset(Dataset):
         if self.transform:
             image = self.transform(image)
         return image.float()
+    
+ChromeCutter("","","")
