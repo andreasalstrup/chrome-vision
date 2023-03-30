6097c42e9ce9b5d57925d93475bf7f541bcbc559
Andreas <andreas090994@gmail.com>
2023-03-30 17:08:10 +0200

    MoCo first aid (#2)
    * Saving MoCo
    
    * MoCo working setup
    
    * Util legend
    
    * MoCo clean up
    
    * Return top1
    
    * Refactor
    
    * Refactor + clean up
    
    * Updated
    
    ---------
    
    Co-authored-by: Martin Algreen <git@mal.green>

---
+++ utils/show_training.py
@@ -0,0 +1,40 @@
+import torch
+import matplotlib.pyplot as plt
+
+class ShowTraining():
+    def __init__(self) :
+        # loss history
+        self.y_loss = {}
+        self.y_loss['train'] = []
+        self.y_loss['val'] = []
+
+        self.top5_rate = {}
+        self.top5_rate['train'] = []
+        self.top5_rate['val'] = []
+
+        self.top1_rate = {}
+        self.top1_rate['train'] = []
+        self.top1_rate['val'] = []
+
+        self.x_epoch = []
+
+        # Create figure
+        fig = plt.figure()
+        self.ax0 = fig.add_subplot(131, title="loss")
+        self.ax1 = fig.add_subplot(132, title="top1acc")
+        self.ax2 = fig.add_subplot(133, title="top5acc")
+
+    def draw_curve(self, current_epoch):
+        self.x_epoch.append(current_epoch)
+        self.ax0.plot(self.x_epoch, torch.Tensor(self.y_loss['train']), 'bo-', label='train')
+        self.ax1.plot(self.x_epoch, torch.Tensor(self.top1_rate['train']), 'bo-', label='train')
+        self.ax2.plot(self.x_epoch, torch.Tensor(self.top5_rate['train']), 'bo-', label='train')
+        if current_epoch == 0:
+            self.ax0.legend()
+            self.ax1.legend()
+            self.ax2.legend()
+    
+    def appendData(self, loss, top1, top5):
+        self.y_loss["train"].append(loss)
+        self.top1_rate["train"].append(top1)
+        self.top5_rate["train"].append(top5)
