cd2838929df4a0a448b609097df2e0c4637cb9de
Andreas Knudsen Alstrup <andreas090994@gmail.com>
2023-06-26 23:31:12 +0200

    Changed comment

--- model/evaluation.py
+++ model/evaluation.py
@@ -20,7 +20,7 @@ def train_step(model: torch.nn.Module,
    # X: image (features)
    for batch, images in enumerate(tqdm(data_loader)):
 
-      # (batch_size, C, H, W)  
+      # (batch_size, C, H, W)
       query_images = images[0].to(device)
       key_images = images[1].to(device)
 
@@ -66,9 +66,7 @@ def test_step(model: torch.nn.Module,
    with torch.inference_mode():
       for batch, (images) in enumerate(tqdm(data_loader)):
 
-         # Add an extra dimension to image (tensor) in batch.
-         # 3rd to 4th dimension
-         # (1, C, H, W)  
+         # (batch_size, C, H, W)
          query_image = images[0].to(device)
          key_image = images[1].to(device)
 
@@ -106,9 +104,7 @@ def eval_model(name: str,
    with torch.inference_mode():
       for batch, (images) in enumerate(tqdm(data_loader)):
 
-         # Add an extra dimension to image (tensor) in batch.
-         # 3rd to 4th dimension
-         # (1, C, H, W)  
+         # (batch_size, C, H, W) 
          query_image = images[0].to(device)
          key_image = images[1].to(device)
 
